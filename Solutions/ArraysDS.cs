2b0b2c57966911519f38014d615f1138afe44080
Italo Santana <flaubert165@gmail.com>
2019-05-25 12:59:55 -0300

    Create ArraysDS.cs

---
+++ Solutions/ArraysDS.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class ArraysDS {
+
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a) {
+
+        int[] result = new int[a.Length];
+        int counter = 0;
+        
+        for(int i = a.Length -1; i >= 0; i--)
+        {
+            result[counter] = a[i]; 
+            counter++;
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
