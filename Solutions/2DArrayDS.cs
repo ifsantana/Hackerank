5a6483cd1c9403cf1022218dcc9c61612a8928eb
Italo Santana <flaubert165@gmail.com>
2019-06-05 09:31:33 -0300

    Create 2DArrayDS.cs

---
+++ Solutions/2DArrayDS.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class 2DArrayDS {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) 
+    {
+        int[] sums = new int[16];
+        int counter = 0;
+
+        for(int i = 0; i <  arr.Count() - 2; i++)
+        {
+            for(int j = 0; j < arr.Count() - 2; j++)
+            {
+                sums[counter] = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                counter++;
+            }
+        }
+       
+      return sums.Max();
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++) {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = hourglassSum(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+
+}
