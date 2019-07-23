d2cc301fdd766c101b18506a3e92167484809803
Italo Santana <flaubert165@gmail.com>
2019-07-23 15:15:26 -0300

    Create BinarySearchTrees.cs

---
+++ Solutions/BinarySearchTrees.cs
@@ -0,0 +1,57 @@
+using System;
+class Node{
+    public Node left,right;
+    public int data;
+    public Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class BinarySearchTrees {
+
+static int getHeight(Node root){
+      //Write your code here
+      int leftHeigth = 0;
+      int rightHeigth = 0;
+
+      if(root == null)
+        return -1;
+
+      if(root.left != null)
+        leftHeigth = 1 + getHeight(root.left);
+
+      if(root.right != null)
+        rightHeigth = 1 + getHeight(root.right);
+
+      return leftHeigth > rightHeigth ? leftHeigth : rightHeigth;
+    }
+
+	static Node insert(Node root, int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args){
+        Node root=null;
+        int T=Int32.Parse(Console.ReadLine());
+        while(T-->0){
+            int data=Int32.Parse(Console.ReadLine());
+            root=insert(root,data);            
+        }
+        int height=getHeight(root);
+        Console.WriteLine(height);
+        
+    }
+}
